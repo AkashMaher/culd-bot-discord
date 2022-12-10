7f1cd181a55bd94e91bb8139852e3339cf60b3ee
Akash <aakashrajp5@gmail.com>
2022-12-11 02:09:16 +0530

    fix

--- index.js
+++ index.js
@@ -208,9 +208,9 @@ client.on(Events.InteractionCreate, async interaction => {
 			if(!checkIfuser(userId).message) return interaction.reply({ content: `The user not found in core team`, ephemeral: true })
 			let msgData = await addPoints(checkIfuser(userId).user.user,{user_id:userId,points:points},databaseName)
 
+			 await interaction.reply({content:"Point Added",ephemeral:true})
 			 log.send({embeds:[msgData],ephemeral:false})
-			 await interaction.deferReply({content:"Point Added",ephemeral:true})
-			 await interaction.editReply({content:"Point Added",ephemeral:true})
+			 
 			//  interaction.reply("Point Added")
 			
 		} else if ( interaction.commandName === 'addtask'){
@@ -221,19 +221,19 @@ client.on(Events.InteractionCreate, async interaction => {
 			
 			let addTaskData = await addTasks(input,databaseName)
 			// console.log(addTaskData)
+			await interaction.reply({content:"Task Added",ephemeral:true})
 			log.send({embeds:[addTaskData], ephemeral: false })
-			 await interaction.deferReply({content:"Task Added",ephemeral:true})
-			 await interaction.editReply({content:"Task Added",ephemeral:true})
+			 
 			
 		} else if ( interaction.commandName === 'view-task'){
 			const task_id = interaction.options._hoistedOptions?.[0].value
 			let input = {user:interaction.user,task_id:task_id}
 			
 			let viewTaskData = await viewTask(input,databaseName)
-			await interaction.deferReply({content:"Point Added",ephemeral:true})
-			 await interaction.editReply({embeds:[viewTaskData], ephemeral: true })
+			 await interaction.reply({embeds:[viewTaskData], ephemeral: true })
 		} else if ( interaction.commandName === 'approve-task'){
 			if(!checkIfAdmin(interaction.user.id).message) return await interaction.reply({content:"You don't have access to approve tasks",ephemeral:true})
+			await interaction.reply({content:"Task Approving",ephemeral:true})
 			const task_id = interaction.options._hoistedOptions?.[0].value
 			let input = {user:interaction.user,task_id:task_id}
 
@@ -241,10 +241,10 @@ client.on(Events.InteractionCreate, async interaction => {
 			// console.log()
 			let msg = viewTaskData.data
 			if(viewTaskData.message !== 'success') return await interaction.reply({content:viewTaskData.message, ephemeral: true })
-			log.send({embeds:[msg], ephemeral: false })/
+			await interaction.editReply({content:"Task Approved",ephemeral:true})
+			log.send({embeds:[msg], ephemeral: false })
 			// await log.send(`Event: Task Add\n Task ID: ${addTaskData.task_id}\n User ID: ${addTaskData.user_id} \n User Name:${addTaskData.user_name}\nTASK DETAILS\n Task Category: ${addTaskData.task_details.category}\n Task Name: ${addTaskData.task_details.task_name}\n Points:${addTaskData.task_details.point}\n\nBy ${interaction.user.tag}`)
-			 await interaction.deferReply({content:"Task Approving",ephemeral:true})
-			 await interaction.editReply({content:"Task Approved",ephemeral:true})
+
 		}
 		 else {
             await command.execute(interaction);
